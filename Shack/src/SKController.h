8ce69cfa3e92df85e35241fc56709b974b25cca8
Chenguang Wang <w3cing@gmail.com>
2012-10-22 20:41:31 -0700

    Initial dumb commit.
    Only implemented a skeleton GUI.

---
+++ Shack/src/SKController.h
@@ -0,0 +1,15 @@
+//
+//  SKController.h
+//  Shack
+//
+//  Created by Chenguang Wang on 10/22/12.
+//  Copyright (c) 2012 Chenguang Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKController : NSObject
+
+- (IBAction)problem:(id)sender;
+
+@end
---
+++ Shack/src/SKController.m
@@ -0,0 +1,17 @@
+//
+//  SKController.m
+//  Shack
+//
+//  Created by Chenguang Wang on 10/22/12.
+//  Copyright (c) 2012 Chenguang Wang. All rights reserved.
+//
+
+#import "SKController.h"
+
+@implementation SKController
+
+- (IBAction)problem:(id)sender {
+    NSLog(@"Problem?");
+}
+
+@end
